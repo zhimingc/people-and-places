5234c3f47b6d379a66b7276007e590d9e29adfa0
Zhiming Chen <zhi.m.ch@gmail.com>
2020-12-10 19:28:29 +1100

    Added dialogue toggling

--- Singletons/DialogueController.gd
+++ Singletons/DialogueController.gd
@@ -5,18 +5,19 @@ var messageBox
 var prompt
 
 var messages = [
-	"What country you from!",
-	"What?",
+	"What [tornado radius=5 freq=6]country[/tornado] you from!",
+	"[shake]What?",
 	"'What' ain't no country I know! Do they speak English in 'What'?",
-	"What?",
-	"English-motherfucker-can-you-speak-it?",
-	"Yes.",
+	"[shake]What?",
+	"[shake rate=25 level=20]English-motherfucker-can-you-speak-it?",
+	"[shake][fade start=0 length=5]Yes...",
 	"Then you understand what I'm saying'?",
 	"Now describe what Marsellus Wallace looks like!",
 	"What?",
 	"Say 'What' again! C'mon, say 'What' again! I dare ya, I double dare ya motherfucker, say 'What' one more goddamn time!"
 	]
 var currentMsg = 0
+var showingMessage = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,12 +28,27 @@ func _ready():
 	hide_message()
 	
 func show_message():
-	messageBox.visible = true
-	toggle_prompt(-1)
+	
+	if !showingMessage:
+		messageBox.visible = true
+		update_bbcode(messages[0])
+		toggle_prompt(-1)
+	else:
+		progress_text()
+	
+	showingMessage = true
 	
 func hide_message():
 	messageBox.visible = false
+	showingMessage = false
 	toggle_prompt(1)
+
+func progress_text():
+	currentMsg = min(messages.size(), currentMsg + 1)
+	update_bbcode(messages[currentMsg])
+
+func update_bbcode(msg):
+	messageBox.get_node("RichText").bbcode_text = msg
 
 func toggle_prompt(show = 0):
 	if show == 0:
